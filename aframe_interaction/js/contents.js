6e259875647325365e51d3c7de39a2d54a58a3c3
shiruco <shiruco621@gmail.com>
2016-12-04 21:22:34 +0900

    for iphone safai

--- aframe_interaction/js/contents.js
+++ aframe_interaction/js/contents.js
@@ -1,15 +1,15 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function() {
-  const pictures = document.querySelectorAll(".picture");
+  var pictures = document.querySelectorAll(".picture");
 
-  const setPictureEvent = function(picture) {
-    let artist = picture.id;
-    let menu = document.querySelector('#' + artist + 'Menu');
-    let menu1 = menu.querySelector(".menu1");
-    let menu2 = menu.querySelector(".menu2");
-    let menu3 = menu.querySelector(".menu3");
-    let close = menu.querySelector(".close");
+  var setPictureEvent = function(picture) {
+    var artist = picture.id;
+    var menu = document.querySelector('#' + artist + 'Menu');
+    var menu1 = menu.querySelector(".menu1");
+    var menu2 = menu.querySelector(".menu2");
+    var menu3 = menu.querySelector(".menu3");
+    var close = menu.querySelector(".close");
 
     menu1.addEventListener('mouseenter',function(){
       menu1.setAttribute('color','#CCC');
@@ -45,22 +45,22 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     picture.addEventListener('mouseenter',function(){
-      let artist = this.id;
+      var artist = this.id;
       if (artist) {
-        let panel = document.querySelector('#' + artist + 'Panel');
+        var panel = document.querySelector('#' + artist + 'Panel');
         panel.setAttribute('visible','true');
       }
     });
     picture.addEventListener('mouseleave',function(){
-      let artist = this.id;
+      var artist = this.id;
       if (artist) {
-        let panel = document.querySelector('#' + artist + 'Panel');
+        var panel = document.querySelector('#' + artist + 'Panel');
         panel.setAttribute('visible','false');
       }
     });
     picture.addEventListener('click',function(){
-      let artist = this.id;
-      let menu = document.querySelector('#' + artist + 'Menu');
+      var artist = this.id;
+      var menu = document.querySelector('#' + artist + 'Menu');
       if(menu.getAttribute('visible')) return;
       menu.querySelector('.menu1').emit('show');
       menu.querySelector('.menu2').emit('show');
@@ -70,8 +70,8 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 
-  let picture;
-  for (let i = 0, pictureNum = pictures.length; i < pictureNum; i++) {
+  var picture;
+  for (var i = 0, pictureNum = pictures.length; i < pictureNum; i++) {
     picture = pictures[i];
     setPictureEvent(picture);
   }
